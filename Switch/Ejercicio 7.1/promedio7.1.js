4b2b5fb7d27f5aa2624023b0d3cb0b3a8234beae
Sebastián <serviciossociologicos@gmail.com>
2020-12-13 14:23:28 -0500

    Se han hecho varios cambios los cuales no se regisrtraran por motivos de tiempo y para evitar distracciones

--- Switch/Ejercicio 7.1/promedio7.1.js
+++ Switch/Ejercicio 7.1/promedio7.1.js
@@ -1,75 +1,84 @@
 //Inicio
-
-document.getElementById('descuento').style.display="none"; 
+document.getElementById('descuento').style.display = "none";
 
 //Declarar variables
 
-var PVP= 50;
-var cantidad = 0;
+var PVP = 50;
+var cantidad;
 var descuento = 0;
 var precioFinal;
 var codPromocion;
 
 
+function Comprar() {
 
-//Ingresar cantidad a comprar.
+  document.getElementById('cantidad').style.display = "none" /*Ocultar contenedor de datos de venta */
+  document.getElementById('descuento').style.display = "block" /*Mostrar contenedor de orden de compra */
 
-cantidad= prompt('Ingresa cantidad');
-//Number(document.getElementById('Compra').value);
+  //Ingresar cantidad a comprar.
+
+  cantidad = Number(document.getElementById('Compra').value);
+
+  switch (true) {
+    case cantidad >= 50:
+      break;
+
+    default:
+      precioFinal = PVP * cantidad;
+      document.getElementById('cantidad').style.display = "none";
+      document.getElementById('titulo2').style.display = "block";
+      document.write("Precio= " + precioFinal + " €");      
+      break;
+  }
+
+}
+
+
+
+function Calcular() {
 
 //Ingresar codigo de promoción.
 
-//codPromocion = document.getElementById('Codigo').value;
 
-//Procedimiento.
 
-//Invocar funcion para cantidad.
- 
-switch(true){
-  case cantidad >= 50:
-  document.write('Debes ingresar codigo de compra para obtener tu descuento');
-    break;
+  //codPromocion = document.getElementById('Codigo').value;
+
+  //Procedimiento.
+
+  //Invocar funcion para cantidad.
+
   
-  default:
-    document.write('no tienes derecho a descuento');
-    break;
+
 }
-
-
-
-
-
 //pasamos a mayúsculas para evitar diferencias al comparar por haber usado minúsculas:
 
-/*codPromocion = codPromocion.toUpperCase();  
+codPromocion = codPromocion.toUpperCase();
 
-switch(codPromocion) {
+switch (codPromocion) {
 
   case "PLUSORO":
-  descuento = 25;
-  break; 
+    descuento = 25;
+    break;
 
-  case "PLATINO33" :
-  descuento = 20;
-  break;
+  case "PLATINO33":
+    descuento = 20;
+    break;
 
-  case "100MAX" :
-  descuento = 15;
-  break; 
+  case "100MAX":
+    descuento = 15;
+    break;
 
-  case "SUPERSILVER" :
-  descuento = 10;
-  break;
+  case "SUPERSILVER":
+    descuento = 10;
+    break;
 
-  case "TOTALBRONCE" :
-  descuento = 5;
-  break;
+  case "TOTALBRONCE":
+    descuento = 5;
+    break;
 
-  default: 
-  alert("No ha introducido código promocional, o es erróneo");
+  default:
+    alert("No ha introducido código promocional, o es erróneo");
 }
 
 precioFinal = PVP - descuento;
-document.write("Precio= " + precioFinal + " €");*/
-
-
+document.write("Precio= " + precioFinal + " €");
